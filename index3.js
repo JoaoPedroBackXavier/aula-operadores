88487d5c2af87670c51892abf6f41c80b2d6e200
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2023-04-26 20:38:27 -0300

    aula operadores

---
+++ index3.js
@@ -0,0 +1,6 @@
+//let nome = prompt("qual seu nome")
+//let aqn = Number(prompt("que ano voce nasceu"))
+//let aa = Number(prompt("que ano é agora"))
+//let i = aa - aqn 
+//let mdi = i > 18
+//console.log("seu nome é", nome, "voce tem", i, "anos. voce é maior de idade", mdi, "voce tera", 2050 - aqn, "anos em 2050")
